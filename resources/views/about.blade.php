dbf68b9d70aa8162393f57e4365297cdaa23085e
azmi <muhdazmi04@gmail.com>
2024-01-28 01:49:04 +0800

    2nd commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+@section('title', 'Registration')
+@section('content')
+  <h1>About Page</h1>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+@section('title', 'Registration')
+@section('content')
+  <h1>Home Page</h1>
+@endsection
